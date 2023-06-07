cbadcfd2b067bc1a7f71b0b8dcb55e5b8e7b55eb
veronicajg <59001019+veronicajg@users.noreply.github.com>
2023-06-06 23:48:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-1ytdbq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-1ytdbq)
---
+++ index.js
@@ -0,0 +1,8 @@
+Import stylesheets
+import './style.css';
+
+
+
+//Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
